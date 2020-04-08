5544666c83d4e41c6426da88e2f61a68c3978a0e
David Passola <dpassola@esolvo.es>
2020-04-08 21:04:08 +0200

    [ADDED] Afegida classe BD [ADDED] Afegida classe Utils

---
+++ js/bd.js
@@ -0,0 +1,41 @@
+class bd {
+    horari = null;
+    dataNaixement = null;
+    util = null;
+
+    constructor() {
+        this.util = new utils();
+        this.carrega();
+    }
+    guarda(json) {
+        if (!this.util.isJSON(json))
+        {
+            return false;
+        }
+        if (json.horari && json.dataNaixement)
+        {
+            localStorage.setItem('horari', JSON.stringify(json.horari));
+            localStorage.setItem('dataNaixement', json.dataNaixement);
+        }
+        return true;
+    }
+    carrega()
+    {
+        if (this.teConfiguracio())
+        {
+            let horariStr = localStorage.getItem('horari');
+            if (this.util.IsJsonString(horariStr))
+            {
+                this.horari = JSON.parse(horariStr);
+            }
+            this.dataNaixement = localStorage.getItem('dataNaixement');
+        }
+    }
+    teConfiguracio() {
+        if(!localStorage.getItem('horari') || !localStorage.getItem('dataNaixement')) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
